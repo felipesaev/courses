7833045c3a302d6dbe2ec933c5c8ef61a339f086
scosta_felipe <scosta_felipe@sicredi.com.br>
2023-05-19 17:47:57 -0300

    feat: create new context and adjust props

--- contextAPI/react-context-1/src/pages/Login/index.js
+++ contextAPI/react-context-1/src/pages/Login/index.js
@@ -2,9 +2,13 @@ import { Button } from "@material-ui/core";
 import { Container, Titulo, InputContainer } from "./styles";
 import { Input, InputLabel, InputAdornment } from "@material-ui/core";
 import { useHistory } from "react-router-dom";
+import { UsuarioContext } from "common/context/Usuario";
+import { useContext } from "react";
 
-function Login({ nome, setNome, saldo, setSaldo }) {
+function Login() {
   const history = useHistory();
+  const { nome, setNome, saldo, setSaldo } = useContext(UsuarioContext);
+
   return (
     <Container>
       <Titulo>Insira o seu nome</Titulo>
@@ -25,7 +29,11 @@ function Login({ nome, setNome, saldo, setSaldo }) {
           startAdornment={<InputAdornment position="start">R$</InputAdornment>}
         />
       </InputContainer>
-      <Button variant="contained" color="primary" onClick={() => history.push('/feira')}>
+      <Button
+        variant="contained"
+        color="primary"
+        onClick={() => history.push("/feira")}
+      >
         Avançar
       </Button>
     </Container>
--- contextAPI/react-context-1/src/routes.js
+++ contextAPI/react-context-1/src/routes.js
@@ -2,26 +2,20 @@ import { BrowserRouter, Switch, Route } from "react-router-dom";
 import Login from "pages/Login";
 import Feira from "./pages/Feira/index";
 import Carrinho from "pages/Carrinho";
-import { useState } from "react";
+import { UsuarioProvider } from "common/context/Usuario";
 
 function Router() {
-  const [nome, setNome] = useState("");
-  const [saldo, setSaldo] = useState(0);
-
   return (
     <BrowserRouter>
       <Switch>
-        <Route exact path="/">
-          <Login
-            nome={nome}
-            setNome={setNome}
-            saldo={saldo}
-            setSaldo={setSaldo}
-          />
-        </Route>
-        <Route exact path="/feira">
-          <Feira />
-        </Route>
+        <UsuarioProvider>
+          <Route exact path="/">
+            <Login />
+          </Route>
+          <Route exact path="/feira">
+            <Feira />
+          </Route>
+        </UsuarioProvider>
         <Route path="/carrinho">
           <Carrinho />
         </Route>
